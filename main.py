3388b8159b0ab983d5812cc4b95db82fbe8b2847
dymytryke <dymytryke@gmail.com>
2023-06-06 15:39:56 +0300

    Calc val refactoring + added interval

--- main.py
+++ main.py
@@ -22,17 +22,39 @@ def build_hist(sample):
     plt.grid(ls=':')
     plt.show()
 
+def calculate_median(sample):
+    sorted_sample = sorted(sample)
+    n = len(sorted_sample)
+    if n % 2 == 0:
+        mid_index_1 = n // 2
+        mid_index_2 = mid_index_1 - 1
+        median = (sorted_sample[mid_index_1] + sorted_sample[mid_index_2]) / 2
+    else:
+        mid_index = n // 2
+        median = sorted_sample[mid_index]
+    return median
+
+def calculate_varience(sample):
+    varience = 0;
+    mean = np.sum(sample) / len(sample)
+    for x in sample:
+        varience += np.square(x - mean)
+    varience = varience / (len(sample) - 1)
+    return varience
+
+
+
 def calc_val(sample):
-    mean = np.mean(sample)
-    median = np.median(sample)
+    mean = np.sum(sample) / len(sample)
+    median = calculate_median(sample)
     mode = max(list(sample), key=list(sample).count)
-    variance = np.var(sample)
-    std_deviation = np.std(sample)
+    variance = calculate_varience(sample)
+    sqrd_deviation = np.sqrt(variance)
     print("Вибіркове середнє:", mean)
     print("Медіана:", median)
     print("Мода:", mode)
     print("Вибіркова дисперсія:", variance)
-    print("Середньоквадратичне відхилення:", std_deviation)
+    print("Середньоквадратичне відхилення:", sqrd_deviation)
 
 def build_box(sample):
     plt.figure(figsize=(6, 6))
@@ -72,6 +94,15 @@ def build_pie(sample):
     ax.set_title('Кругова діаграма')
     plt.show()
 
+def calculate_and_build_interval(sample):
+    mean = np.mean(sample)
+    n = len(sample)
+    z = 1.96
+    s = np.sqrt(calculate_varience(sample))
+    low_point = mean - z * (s / np.sqrt(n))
+    high_point = mean + z * (s / np.sqrt(n))
+    print("двосторонній довірчий інтервал на математичне сподівання: \n"
+          "Нижня межа: " + str(low_point) + "\nВерхня межа: " + str(high_point))
 
 if __name__ == '__main__':
     sample = sps.norm.rvs(loc=0, scale=2.1, size=127)
@@ -81,5 +112,6 @@ if __name__ == '__main__':
     build_paretto(sample)
     build_pie(sample)
     calc_val(sample)
+    calculate_and_build_interval(sample)
 
 
